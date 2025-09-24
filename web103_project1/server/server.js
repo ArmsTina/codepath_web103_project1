1f9e085969401304bd60169036942396ca79a6b1
ArmsTina <didehddud1005@gmail.com>
2025-09-23 21:26:54 -0700

    server added?

---
+++ web103_project1/server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import operatorsRouter from "./routes/operators.js";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use("/public", express.static("./public"));
+app.use("/scripts", express.static("./public/scripts"));
+app.use("/operators", operatorsRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening on http://localhost:${PORT}`);
+});
